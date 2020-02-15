fcee725d57a2b8b4a35bce6bca30407483fc38a6
Marvin Reyes <reyemarv@gmail.com>
2020-02-15 05:13:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
     array = self.split
-    array.count{ |x| x.sentenc? || x.question? || x.exclamation?}
+    array.count{ |x| x.sentence? || x.question? || x.exclamation?}
   end
 end
